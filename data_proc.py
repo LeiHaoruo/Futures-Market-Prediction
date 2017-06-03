2b5ef08427f99191a2e8baeb014641b79dad386c
LeiHaoruo <515106143@qq.com>
2017-06-03 20:56:54 +0800

    lstm and data process

---
+++ data_proc.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+
+attributes = ['InstrumentID', 'TradingDay', 'UpdateTime', 'UpdateMillisec', 'LastPrice', 'Volume', 'LastVolume', 'Turnover', 'LastTurnover', 'AskPrice5', 'AskPrice4', 'AskPrice3', 'AskPrice2', 'AskPrice1', 'BidPrice1', 'BidPrice2', 'BidPrice3', 'BidPrice4', 'BidPrice5', 'AskVolume5', 'AskVolume4', 'AskVolume3', 'AskVolume2', 'AskVolume1', 'BidVolume1', 'BidVolume2', 'BidVolume3', 'BidVolume4', 'BidVolume5', 'OpenInterest', 'UpperLimitPrice', 'LowerLimitPrice']
+is_drop = [1,1,1,1,0,0,0,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,0,0,0]
+TimeSlice = 20
+
+def get_label(data,time):
+	l = len(data)
+	cur = time-1
+	for i in range(time,l):
+		if(data[cur][-1]<data[i][-1]):
+			return 1  #up
+		if(data[cur][-1]>data[i][-1]):
+			return 0 #down
+	return 0 #no change for all peroids
+
+def load_data(path):
+	df = pd.read_csv(path)
+	for i in range(len(is_drop)):
+		if(is_drop[i]==1):
+			df=df.drop(attributes[i],1)
+	df['PredictPrice']=(df['AskPrice1']+df['BidPrice1'])
+	data = df.values
+	data_ = np.delete(data,-1,axis=1)
+	x_ =data_[0:TimeSlice]
+	label= get_label(data,TimeSlice)
+	y_ = np.array([label])
+	for i in range(TimeSlice,len(data)-TimeSlice,TimeSlice):
+		features = data_[i:i+TimeSlice]
+		x_= np.vstack((x_,features))
+		label= get_label(data,i+TimeSlice)
+		y_ = np.append(y_,label)
+	
+	mm_scaler = preprocessing.MinMaxScaler()	
+	x_ = mm_scaler.fit_transform(x_)	
+	x_ = np.reshape(x_, (x_.shape[0]/20,20,x_.shape[1]))
+	train_size = int(len(x_)*0.8)
+	x_train = x_[0:train_size]
+	y_train = y_[0:train_size]
+	x_test = x_[train_size:]
+	y_test = y_[train_size:]
+	
+	return x_train,y_train,x_test,y_test
---
+++ lstm.py
@@ -0,0 +1,24 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM
+import numpy as np
+import data_proc
+
+def trainModel(X_train,Y_train,X_test,Y_test):
+	model = Sequential()
+	#model.add(Embedding(max_features, 256, input_length=maxlen))
+	model.add(LSTM(128, input_shape=(20,12)))
+	model.add(Dropout(0.2))
+	model.add(Dense(1, activation='sigmoid'))
+
+	model.compile(loss='binary_crossentropy', optimizer='rmsprop',metrics=['accuracy'])
+
+	model.fit(X_train, Y_train, batch_size=16, epochs=15,validation_data=(X_test,Y_test))
+	res = model.evaluate(X_test, Y_test, batch_size=16)
+	print ("score:",res[0])
+	print ("acc",res[1])
+
+if __name__=='__main__':
+	x_train,y_train,x_test,y_test = data_proc.load_data('TK_m0000[s20170404 00205000_e20170414 00153000]20170410_1755_46.csv')
+	trainModel(x_train,y_train,x_test,y_test)
