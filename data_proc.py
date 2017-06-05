f827bbcb201e62bb25849ce6b0834635d7f65d46
LeiHaoruo <515106143@qq.com>
2017-06-05 20:31:07 +0800

    train on multiple files

--- data_proc.py
+++ data_proc.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
-
+import os
 attributes = ['InstrumentID', 'TradingDay', 'UpdateTime', 'UpdateMillisec', 'LastPrice', 'Volume', 'LastVolume', 'Turnover', 'LastTurnover', 'AskPrice5', 'AskPrice4', 'AskPrice3', 'AskPrice2', 'AskPrice1', 'BidPrice1', 'BidPrice2', 'BidPrice3', 'BidPrice4', 'BidPrice5', 'AskVolume5', 'AskVolume4', 'AskVolume3', 'AskVolume2', 'AskVolume1', 'BidVolume1', 'BidVolume2', 'BidVolume3', 'BidVolume4', 'BidVolume5', 'OpenInterest', 'UpperLimitPrice', 'LowerLimitPrice']
 is_drop = [1,1,1,1,0,0,0,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,0,0,0]
 TimeSlice = 20
@@ -16,7 +16,29 @@ def get_label(data,time):
 			return 0 #down
 	return 0 #no change for all peroids
 
-def load_data(path):
+#is_fit: true for training, false for testing
+#file_set: index of files we use
+def generator_from_path(path,file_set):
+    steps = 32
+    count = 0
+    prefix_inx = 1
+    f_csv_list = []
+    for parent, dirnames, filenames in os.walk(path):
+        for p_inx in file_set:
+	    for f in filenames:
+		if f[f.rfind('_')+1:f.rfind('.')]==str(p_inx) and f.find('TK')>=0:
+	            f_csv_list.append(f)
+	print f_csv_list
+	while 1:
+	    for f_csv in f_csv_list:
+	        print "reading file: "+f_csv
+	        X,Y,x_none,y_none = load_data(path+f_csv,True)
+	        print "processing file: "+f_csv
+	        for i in range(0,Y.shape[0]-steps,steps):
+	            yield(X[i:i+steps],Y[i:i+steps])
+
+#no_separate: True for no separate it into train and test parts
+def load_data(path,no_separate):
 	df = pd.read_csv(path)
 	for i in range(len(is_drop)):
 		if(is_drop[i]==1):
@@ -24,6 +46,7 @@ def load_data(path):
 	df['PredictPrice']=(df['AskPrice1']+df['BidPrice1'])
 	data = df.values
 	data_ = np.delete(data,-1,axis=1)
+	'''
 	x_ =data_[0:TimeSlice]
 	label= get_label(data,TimeSlice)
 	y_ = np.array([label])
@@ -32,13 +55,25 @@ def load_data(path):
 		x_= np.vstack((x_,features))
 		label= get_label(data,i+TimeSlice)
 		y_ = np.append(y_,label)
-	
-	mm_scaler = preprocessing.MinMaxScaler()
-	st_scaler = preprocessing.StandardScaler().fit(x_)	
+	'''
+	index = path[path.rfind('_')+1:path.rfind('.')]
+	dirs = path[0:path.rfind('/')+1]
+	y_ = pd.read_csv(dirs+'file_'+index+'.csv')
+	y_ = y_.values
+	y_ = np.delete(y_,0,axis=1)
+	#print "y element:",y_[:10]
+	#mm_scaler = preprocessing.MinMaxScaler()
+	st_scaler = preprocessing.StandardScaler().fit(data_)
+	x_ = st_scaler.transform(data_)
+	d_size = x_.shape[0]/TimeSlice*TimeSlice
+	x_ = np.reshape(x_[0:d_size], (x_.shape[0]/TimeSlice,TimeSlice,x_.shape[1]))
+	print "x's shape",x_.shape, "y's shape",y_.shape	
 	#x_ = mm_scaler.fit_transform(x_)	
-	x_ = st_scaler.transform(x_)
-	x_ = np.reshape(x_, (x_.shape[0]/20,20,x_.shape[1]))
+	#x_ = st_scaler.transform(x_)
+	#x_ = np.reshape(x_, (x_.shape[0]/20,20,x_.shape[1]))
 	train_size = int(len(x_)*0.8)
+	if no_separate:
+	    return x_,y_,None,None
 	x_train = x_[0:train_size]
 	y_train = y_[0:train_size]
 	x_test = x_[train_size:]
--- lstm.py
+++ lstm.py
@@ -4,6 +4,21 @@ from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
 import numpy as np
 import data_proc
+import os
+
+def lstm_with_generator(numFile):
+    model = Sequential()
+    #model.add(Embedding(max_features, 256, input_length=maxlen))
+    model.add(LSTM(128, input_shape=(20,12)))
+    model.add(Dropout(0.2))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop',metrics=['accuracy'])
+
+    model.fit_generator(data_proc.generator_from_path("marketing_data/m0000/", file_set = [1,2,3,4]), steps_per_epoch=1000, epochs=15, validation_data =
+         data_proc.generator_from_path("marketing_data/m0000/", [5]), validation_steps = 100)
+    res = model.evaluate_generator(data_proc.generator_from_path("marketing_data/m0000/", [5]), steps=300)
+    print ("score:",res[0])
+    print ("acc",res[1])
 
 def lstm_train(X_train,Y_train,X_test,Y_test):
 	model = Sequential()
@@ -20,5 +35,6 @@ def lstm_train(X_train,Y_train,X_test,Y_test):
 	print ("acc",res[1])
 
 if __name__=='__main__':
-	x_train,y_train,x_test,y_test = data_proc.load_data('TK_m0000[s20170404 00205000_e20170414 00153000]20170410_1755_46.csv')
-	lstm_train(x_train,y_train,x_test,y_test)
+	lstm_with_generator(5)
+	#x_train,y_train,x_test,y_test = data_proc.load_data('marketing_data/m0000/TK_m0000[s20170404 00205000_e20170414 00153000]20170410_1755_46.csv')
+	#lstm_train(x_train,y_train,x_test,y_test)
